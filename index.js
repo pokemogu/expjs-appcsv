c68c5f75552eecbf31cd9228c23af6efa2cb7d04
YAMAHATA Kenichiro <pokemogu@gmail.com>
2021-03-26 08:54:13 +0900

    アップロードファイルをメモリ上で処理するように修正

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
-const path = require('path');
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
+const path = require('path')
+const stream = require('stream');
+const express = require('express')
+const multer = require('multer')
+const fs = require('fs')
 const parse = require("csv-parse/lib/sync");
-const stringify = require('csv-stringify/lib/sync');
+const stringify = require('csv-stringify/lib/sync')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 app.set('view engine', 'ejs');
 
-const rooturl = 'https://personcropping.jp.ngrok.io/';
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
-//const upload = multer({ dest: 'uploads/' })
-const upload = multer({ storage: storage });
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.get('/', (request, response) => response.redirect('/upload'));
 
-app.get('/upload', (req, res) => res.sendFile(path.join(__dirname, 'public/upload.html')));
+app.get('/upload', (req, res) => res.sendFile(path.join(__dirname, 'public/upload.html')))
 
 app.post('/upload', upload.array('csvfile', 2), function (req, res) {
   const dirpath = path.join(__dirname, 'uploads');
-  const infilepath1 = path.join(dirpath, req.files[0].originalname);
-  const infilepath2 = path.join(dirpath, req.files[1].originalname);
   const outfilepath = path.join(dirpath, 'result.csv');
 
-  const csv1 = fs.readFileSync(infilepath1);
-  const csv2 = fs.readFileSync(infilepath2);
+  const csv1 = req.files[0].buffer;
+  const csv2 = req.files[1].buffer;
 
   const records1 = parse(csv1, {
     columns: false,
@@ -68,8 +57,6 @@ app.post('/upload', upload.array('csvfile', 2), function (req, res) {
   fs.writeFileSync(outfilepath, data);
 
   res.render('result', { result_csvfile: '/uploads/result.csv' });
-  fs.unlinkSync(infilepath1);
-  fs.unlinkSync(infilepath2);
 })
 
 app.listen(port, function () {
